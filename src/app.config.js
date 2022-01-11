2ca48f6086ec7813169bcb47b47669ae5397753a
junchen.yang <junchen.yang@ke.com>
2022-01-11 23:27:12 +0800

    init

---
+++ src/app.config.js
@@ -0,0 +1,41 @@
+export default {
+  pages: [
+    'pages/index/index',
+    'pages/exchange/index',
+    'pages/mine/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    list: [
+      {
+        pagePath: "pages/index/index",
+        text: "首页",
+        iconPath: 'assets/home.png',
+        selectedIconPath: 'assets/home-active.png',
+        color: '#666666',
+        selectedColor: '#222222'
+      },
+      {
+        pagePath: "pages/exchange/index",
+        text: "兑换",
+        iconPath: 'assets/exchange.png',
+        selectedIconPath: 'assets/exchange-active.png',
+        color: '#666666',
+        selectedColor: '#222222'
+      },
+      {
+        pagePath: "pages/mine/index",
+        text: "我的",
+        iconPath: 'assets/mine.png',
+        selectedIconPath: 'assets/mine-active.png',
+        color: '#666666',
+        selectedColor: '#222222'
+      }
+    ]
+  },
+}
---
+++ src/pages/index/index.jsx
@@ -0,0 +1,53 @@
+import { Component } from 'react'
+import { View, Text, Image } from '@tarojs/components'
+import './index.less'
+import home2Img from '@/assets/home/home-2.png'
+import home1Img from '@/assets/home/home-1.png'
+import home3Img from '@/assets/home/home-3.png'
+
+export default class Index extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false
+    };
+  }
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  getImg () {
+    this.setState({showModal: true});
+  }
+
+  render () {
+    const {showModal} = this.state;
+    console.log('showModal= ============>', this.state)
+    return (
+      <View className='home'>
+        <Image src={home1Img} className='home-img-1'></Image>
+        <Image src={home2Img} className='home-img-2'></Image>
+        
+        <View className='get-btn' onClick={() => this.getImg()}>
+          <Image src={home3Img} className='home-img-3'></Image>
+          立即领取
+        </View>
+
+        {
+          showModal ? (
+            <View>
+              hello
+            </View>
+          ) : null
+        }
+      </View>
+    )
+  }
+}
